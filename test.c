a33f14532c2f858c5771d083f881aa8105ed7d88
LiuZhengWei <982630332@qq.com>
2022-08-07 12:33:18 +0800

    test.c hot-fixed

--- test.c
+++ test.c
@@ -1,6 +1,6 @@
 hello world! hello world!  Git
 hello world! hello world!  333
-hello world! hello world!
+hello world! hello world!  bug-fixed
 hello world! hello world!
 hello world! hello world!
 hello world! hello world!
